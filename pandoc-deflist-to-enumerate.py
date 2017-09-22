73a36e02b31e509395be1d6d86aac4881ea638a7
Chris <christopher.e.chandler@gmail.com>
2017-09-22 15:51:56 -0400

    added html output for web cv

--- pandoc-deflist-to-enumerate.py
+++ pandoc-deflist-to-enumerate.py
@@ -19,30 +19,58 @@ end_enumerate = [pf.RawBlock('latex', '\\end{enumerate}')]
 begin_item = [pf.RawInline('latex', '\\item[')]
 end_item = [pf.RawInline('latex', '] ')]
 
+begin_row = [pf.RawBlock('html', '<div class="row mb-2">')]
+begin_term_column = [pf.RawBlock('html', '<div class="col-2">')]
+begin_definition_column = [pf.RawBlock('html', '<div class="col-10">')]
+end_div = [pf.RawBlock('html', '</div>')]
+begin_definition_para = [pf.RawBlock('html', '<p class="mb-0">')]
+end_definition_para = [pf.RawBlock('html', '</p>')]
+
 def list2items(key, val, fmt, meta):
     if 'DefinitionList' != key:
         return None
-    return_list = list(begin_enumerate)  # clone
-    for pair in val:
-        (term, definitions) = pair
-        # construct \item[term]
-        item = begin_item + term + end_item
-        # prepend \item[term] to (first) definition
-        # get first para in first definition
-        block = definitions[0][0]
-        # check that it really is a para (or plain) or bail out
-        if not isinstance(block, dict) or not block['t'] in ('Para', 'Plain'):
-            sys.stderr.write(
-                "Topic definitions must be plain paragraphs, not {0}!\n".format(block['t']))
-            sys.exit(255)
-        # the actual prepending
-        block['c'] = item + block['c']
-        # each definition is a list of blocks
-        # merge each of them into the return list
-        for definition in definitions:
-            return_list = return_list + definition
-    # append \end{enumerate} to the return list and return it
-    return return_list + end_enumerate
+    elif fmt == "latex":
+        return_list = list(begin_enumerate)  # clone
+        for pair in val:
+            (term, definitions) = pair
+            # construct \item[term]
+            item = begin_item + term + end_item
+            # prepend \item[term] to (first) definition
+            # get first para in first definition
+            block = definitions[0][0]
+            # check that it really is a para (or plain) or bail out
+            if not isinstance(block, dict) or not block['t'] in ('Para', 'Plain'):
+                sys.stderr.write(
+                    "Topic definitions must be plain paragraphs, not {0}!\n".format(block['t']))
+                sys.exit(255)
+            # the actual prepending
+            block['c'] = item + block['c']
+            # each definition is a list of blocks
+            # merge each of them into the return list
+            for definition in definitions:
+                return_list = return_list + definition
+        # append \end{enumerate} to the return list and return it
+        return return_list + end_enumerate
+    elif fmt == "html":
+        output = [pf.RawBlock('html', "<div></div>")]
+        for pair in val:
+            (term, definitions) = pair
+            output += begin_row + begin_term_column + [pf.Plain(term)] + end_div
+            output += begin_definition_column
+            for definition in definitions:
+                output += begin_definition_para + definition + end_definition_para
+            output += end_div
+            output += end_div
+        return output
+
+# <div class="row">
+#   <div class="col-2">2017</div>
+#   <div class="col-10">
+#     <p class="mb-0">Ph.D. in Composition, Eastman School of Music, Rochester, NY</p>
+#     <p class="mb-0"><em>Studied with:</em> Robert Morris, Carlos Sanchez-Gutierrez, Ricardo Zohn-Muldoon</p>
+#     <p class="mb-0"><em>Dissertation:</em> Recontextualization and Variation: Associative Organization in Hans Abrahamsen's <em>Walden</em> and <em>Wald</em></p>
+#   </div>
+# </div>
 
 
 # def filter_func(key, val, fmt, meta):
